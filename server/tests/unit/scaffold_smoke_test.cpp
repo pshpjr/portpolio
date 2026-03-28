bc8cfe61db1ccd52848aa3cf0c6a5e25a2356736
pshpjr <pshpjr@naver.com>
2026-03-28 10:50:30 +0900

    안 쓰는 파일 제거

--- server/tests/unit/scaffold_smoke_test.cpp
+++ server/tests/unit/scaffold_smoke_test.cpp
@@ -1,10 +1,5 @@
 #include "02_config/server_config.h"
-#include "03_core/combat/combat_system.h"
-#include "03_core/inventory/inventory_manager.h"
-#include "03_core/stat/stat_calculator.h"
-#include "04_service/dungeon/dungeon_instance.h"
-#include "04_service/matchmaking/matchmaking_service.h"
-#include "04_service/party/party_manager.h"
+#include "types/not_null.h"
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/thread_pool.hpp>
@@ -13,50 +8,9 @@
 
 #include <gtest/gtest.h>
 
-TEST(ScaffoldSmokeTest, BootstrapsServerApp) {
-    portpolio::config::ServerConfig config{};
-    portpolio::core::combat::CombatSystem combatSystem(config);
-    portpolio::core::inventory::InventoryManager inventoryManager;
-    portpolio::core::stat::StatCalculator statCalculator;
-    portpolio::service::dungeon::DungeonInstance dungeonInstance(combatSystem);
-    portpolio::service::party::PartyManager partyManager(inventoryManager);
-    portpolio::service::matchmaking::MatchmakingService matchmakingService(
-        statCalculator);
-    boost::asio::io_context ioContext;
-    boost::asio::thread_pool contentPool(1);
-    portpolio::network::executor::NetworkExecutor networkExecutor(
-        ioContext.get_executor(),
-        contentPool.get_executor());
-    portpolio::network::packet::PacketHandler packetHandler(
-        dungeonInstance,
-        networkExecutor);
-    portpolio::network::session::SessionManager sessionManager(
-        partyManager,
-        networkExecutor);
-    portpolio::network::aoi::AoiGrid aoiGrid(matchmakingService);
-    portpolio::runtime::ServerApp app(
-        networkExecutor,
-        packetHandler,
-        sessionManager,
-        aoiGrid);
+TEST(LibIntegrationTest, IncludesLibHeaderThroughServerTarget) {
+    auto value = psh::lib::types::NotNull<int>::Make(42);
 
-    EXPECT_TRUE(app.boot());
-}
-
-TEST(NetworkExecutorTest, DispatchesIoAndContentSeparately) {
-    boost::asio::io_context ioContext;
-    boost::asio::thread_pool contentPool(1);
-    portpolio::network::executor::NetworkExecutor networkExecutor(
-        ioContext.get_executor(),
-        contentPool.get_executor());
-    std::atomic_int executed{0};
-
-    networkExecutor.dispatchIo([&executed]() { ++executed; });
-    networkExecutor.dispatchContent([&executed]() { ++executed; });
-
-    ioContext.run();
-    contentPool.join();
-
-    EXPECT_TRUE(networkExecutor.separated());
-    EXPECT_EQ(executed.load(), 2);
+    ASSERT_TRUE(static_cast<bool>(value.to_weak().lock()));
+    EXPECT_EQ(*value, 42);
 }
