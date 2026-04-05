c08ce8f0b667735f7c0adfe7317d08a1529ebd59
pshpjr <pshpjr@naver.com>
2026-04-05 18:03:27 +0900

    imgui 추가

---
+++ client/Plugins/ImGui-Main/Source/ImGui/ImGui.Build.cs
@@ -0,0 +1,68 @@
+﻿using UnrealBuildTool;
+
+public class ImGui : ModuleRules
+{
+	protected virtual bool WithImPlot => true;
+	protected virtual bool WithNetImGui => true;
+
+	public ImGui(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new[]
+		{
+			"Core",
+			"ImGuiLibrary",
+		});
+
+		PrivateDependencyModuleNames.AddRange(new[]
+		{
+			"ApplicationCore",
+			"InputCore",
+			"Slate",
+			"SlateCore"
+		});
+
+		if (Target.bCompileAgainstEngine)
+		{
+			PrivateDependencyModuleNames.AddRange(new[]
+			{
+				"CoreUObject",
+				"Engine"
+			});
+		}
+
+		if (Target.bBuildEditor)
+		{
+			PrivateDependencyModuleNames.AddRange(new[]
+			{
+				"MainFrame",
+				"UnrealEd"
+			});
+		}
+
+		PublicDefinitions.Add("IMGUI_USER_CONFIG=\"ImGuiConfig.h\"");
+
+		if (WithImPlot)
+		{
+			PublicDependencyModuleNames.Add("ImPlotLibrary");
+			PublicDefinitions.Add("IMPLOT_API=IMGUI_API");
+			PublicDefinitions.Add("WITH_IMPLOT=1");
+		}
+		else
+		{
+			PublicDefinitions.Add("WITH_IMPLOT=0");
+		}
+
+		if (WithNetImGui)
+		{
+			PublicDependencyModuleNames.Add("NetImGuiLibrary");
+			PublicDefinitions.Add("NETIMGUI_API=IMGUI_API");
+			PublicDefinitions.Add("WITH_NETIMGUI=1");
+		}
+		else
+		{
+			PublicDefinitions.Add("WITH_NETIMGUI=0");
+		}
+	}
+}
